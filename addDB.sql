5b5595d00e760fa9f04896ea26d4f788cff380a5
PA-Delaunay <40235634+PA-Delaunay@users.noreply.github.com>
2019-03-06 10:09:27 +0100

    Update addDB.sql

--- addDB.sql
+++ addDB.sql
@@ -1,1 +1,15 @@
 
+//Ajout de tables Chambres
+
+INSERT INTO Chambre(typeChambre, nbPlaceLit, prixJournalier) VALUES
+					("Chambre Double", 2, 80);
+
+INSERT INTO Chambre(typeChambre, nbPlaceLit, prixJournalier) VALUES
+					("Chambre Premium", 2, 160);
+
+INSERT INTO Chambre(typeChambre, nbPlaceLit, prixJournalier) VALUES
+					("Suite", 4, 240);
+
+INSERT INTO Chambre(typeChambre, nbPlaceLit, prixJournalier) VALUES
+					("Suite Presidentielle", 6, 450);
+
