6f688c166e6e45fdcd0b37399d740f19164ecd8f
CarlaC <carla.candiotti@hotmail.fr>
2019-03-06 11:49:13 +0100

    ajout projet 2

---
+++ ProjetHotelUtiliseSW/src/hotel/HotelUtilise.java
@@ -0,0 +1,10 @@
+package hotel;
+
+public class HotelUtilise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ProjetHotelUtiliseSW/test/hotel/HotelUtiliser.java
@@ -0,0 +1,10 @@
+package hotel;
+
+public class HotelUtiliser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
