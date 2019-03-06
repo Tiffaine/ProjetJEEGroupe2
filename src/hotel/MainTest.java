82668e9519b1fefe6646b7122af05d881a8455a1
CarlaC <carla.candiotti@hotmail.fr>
2019-03-06 11:44:11 +0100

    test

---
+++ src/hotel/MainTest.java
@@ -0,0 +1,10 @@
+package hotel;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ test/hotel/TestMain.java
@@ -0,0 +1,10 @@
+package hotel;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
