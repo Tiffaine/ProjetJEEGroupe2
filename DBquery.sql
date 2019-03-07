12784741017f201216eaebc3463d1658eda33721
PA-Delaunay <40235634+PA-Delaunay@users.noreply.github.com>
2019-03-07 16:27:33 +0100

    Update DBquery.sql
    Rédaction et validation des requetes de réservation,  paiement et d'annulations

--- DBquery.sql
+++ DBquery.sql
@@ -1,8 +1,7 @@
 
+--Trouver une chambre à partir d'une chambre demandée par le client
 
---trouver une chambre à partir d'une chambre demandée par le client
-
-sql_query = 'SELECT * FROM Chambre c WHERE c.typeChambre LIKE "%'+typeChambre_param+'%"';
+sql_query = 'SELECT c.* FROM Chambre c, Reservation r WHERE c.typeChambre LIKE "%'+typeChambre_param+'%"';
 if(nbPlaceLit_param != 0){
 	sql_query+ = ' and c.nbPlaceLit = '+ String.valueOf(nbPlace_param);
 }
@@ -12,7 +11,33 @@ if(prixMin_param != 0){
 if(prixMax_param != 0){
 	sql_query+= ' and c.prixJournalier <= '+ String.valueOf(prixMax_param);
 }
+sql_query+= ' and c.idChambre = r.idChambre and (r.dateDeb > "'+dateFin_param+'" or r.dateFin < "'+dateDeb_param+'");';
 
--- Réserver une chambre
 
-'INSERT INTO Reservation(idChambre, idClient, dateDeb, dateFin, nbPlaces, paiementEffectue) VALUES
+-- Réserver une chambre (Validé)
+
+"INSERT INTO Reservation (
+				idChambre, 
+				idClient, 
+				dateDeb, 
+				dateFin, 
+				nbPlaces, 
+				paiementEffectue) 
+		VALUES ("
+				+idChambre_param+","
+				+idClient_param+",'"
+				+dateDeb_param+"','"
+				+dateFin_param+"',"
+				+nbPlaces_param+",
+				'0');"
+
+
+-- Payer une chambre (Validé)
+
+"UPDATE Reservation SET paiementEffectue = '1' WHERE idReservation = "+codeReservation+";"
+
+
+-- Annuler une réservation (Validé)
+
+'DELETE FROM Reservation WHERE idReservation = '+codeReservation+';'
+
